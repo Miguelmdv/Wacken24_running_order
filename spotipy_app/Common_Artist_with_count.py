c5529822822c7739c4fed193dc401ac4d9f222c6
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2024-07-15 00:31:39 +0200

    Fixed custom list creation in spotify

--- spotipy_app/Common_Artist_with_count.py
+++ spotipy_app/Common_Artist_with_count.py
@@ -13,9 +13,13 @@ def run():
     df_band_list = pd.read_csv(band_list_path)
     df_liked_artists = pd.read_csv(liked_artists_path)
 
-    df_common_count = pd.merge(df_band_list, df_liked_artists, on="Artista", how="left").sort_values(by='Artista').reset_index(drop=True) ## QUITAR EL HOW=LEFT PARA TENER EL DF LIMPIO
+    df_common_count = pd.merge(df_band_list, df_liked_artists, on="Artista").sort_values(by='Artista').reset_index(drop=True)
+    df_common_count_all = pd.merge(df_band_list, df_liked_artists, on="Artista", how="left").sort_values(by='Artista').reset_index(drop=True) 
+    ## QUITAR EL HOW=LEFT PARA TENER EL DF LIMPIO
 
-    wacken_comun_path = data_dir / "liked_artists_wacken_with_count.csv"
-    df_common_count.to_csv(wacken_comun_path, index=False)
+    wacken_common_path = data_dir / "liked_artists_wacken_with_count.csv"
+    all_wacken_common_path = data_dir / "all_liked_artists_wacken_with_count.csv"
+    df_common_count.to_csv(wacken_common_path, index=False)
+    df_common_count_all.to_csv(all_wacken_common_path, index=False)
     
-    print(f"La lista de artistas y el número de canciones que te gustan del wacken se ha guardado en {wacken_comun_path}")
+    print(f"La lista de artistas y el número de canciones que te gustan del wacken se ha guardado en {wacken_common_path}")
