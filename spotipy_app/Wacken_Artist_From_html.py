b03bfefb64e939cc2091715220b2c23ac2a3a9b3
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2024-07-19 23:20:57 +0200

    Fix HTML band_list

--- spotipy_app/Wacken_Artist_From_html.py
+++ spotipy_app/Wacken_Artist_From_html.py
@@ -17,40 +17,18 @@ def run():
     # Analizar el HTML con BeautifulSoup
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Encontrar el div con id="artistlist"
-    artist_list_div = soup.find('div', {'id': 'artistlist'})
+    # Extraer todos los elementos <h5> con la clase y atributo especificado
+    artist_elements = soup.find_all('h5', class_='card-title text-center')
 
-    # Encontrar todos los divs dentro del div con id="artistlist"
-    inner_divs = artist_list_div.find_all('div')
-
-    # Conjunto para almacenar los nombres de las bandas únicos
-    band_names_set = set()
-
-    # Iterar sobre los divs internos
-    for div in inner_divs:
-        # Encontrar las etiquetas 'a' dentro de cada div
-        a_tags = div.find_all('a')
-        # Iterar sobre las etiquetas 'a'
-        for a_tag in a_tags:
-            # Encontrar las etiquetas h5 dentro de cada etiqueta 'a'
-            h5_tags = a_tag.find_all('h5')
-            # Iterar sobre las etiquetas h5 y obtener el texto
-            for h5_tag in h5_tags:
-                # Obtener el texto de la etiqueta h5
-                band_name = h5_tag.get_text(strip=True)
-                # Verificar si el nombre de la banda no contiene las cadenas "Band Name" o "Batlle"
-                if "Metal Battle" not in band_name and "40" not in band_name and "Anniversary" not in band_name and "Show" not in band_name:
-                    # Agregar el nombre de la banda al conjunto
-                    band_names_set.add(band_name)
-
-    # Convertir el conjunto a una lista
-    band_names = list(band_names_set)
+    # Crear una lista con el texto de cada elemento encontrado
+    band_names = [element.get_text() for element in artist_elements]
     
     # Ruta completa de la carpeta "data"
     csv_file = Path("data/band_list.csv")
+    
     # Si la carpeta "data" no existe, crearla
     csv_file.parent.mkdir(parents=True, exist_ok=True)
-
+    
     # Crear un DataFrame con los data y guardarlos en un archivo CSV
     df = pd.DataFrame(band_names, columns=['Artista'])
     df.to_csv(csv_file, index=False)
