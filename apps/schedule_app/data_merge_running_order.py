005975f3c2505466cad50db8fa4dfaa9c5e2ab4e
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2024-07-21 18:10:51 +0200

    Fix reorganize()

--- apps/schedule_app/data_merge_running_order.py
+++ apps/schedule_app/data_merge_running_order.py
@@ -4,17 +4,25 @@ from pathlib import Path
 def reorganize(df : pd.DataFrame):
     # Convertir la columna 'Hora_Inicio' a formato datetime
     df['Hora_Inicio'] = pd.to_datetime(df['Hora_Inicio'], format='%H:%M').dt.time
+    df['Hora_Fin'] = pd.to_datetime(df['Hora_Fin'], format='%H:%M').dt.time
 
     # Crear la columna 'buffer'
     df['buffer'] = df['Hora_Inicio'].apply(lambda x: 1 if x >= pd.to_datetime('00:00').time() and x < pd.to_datetime('03:00').time() else 0)
 
+    # Definir el orden de los días de la semana
+    dias_ordenados = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+    df['Día'] = pd.Categorical(df['Día'], categories=dias_ordenados, ordered=True)
+    
     # Ordenar el DataFrame por 'Día', 'buffer', y 'Hora_Inicio'
     df_sorted = df.sort_values(by=['Día', 'buffer', 'Hora_Inicio'], ascending=[True, True, True])
 
     # Eliminar la columna 'buffer' si ya no es necesaria
     df_sorted = df_sorted.drop(columns=['buffer'])
     
-    return df
+    # Resetear los índices
+    df_sorted = df_sorted.reset_index(drop=True)
+
+    return df_sorted
 
 def data_merge(schedule_df : pd.DataFrame, songs_df: pd.DataFrame, direction : str, complete_rute : Path, people : list, drop = False):
     
@@ -24,7 +32,7 @@ def data_merge(schedule_df : pd.DataFrame, songs_df: pd.DataFrame, direction : s
     # Convertir la columna 'Numero de Canciones' a enteros, reemplazar todos los valores excepto los vacíos o los 0 por 1 y renombrarlo
     merged_df[people[0]] = merged_df['Numero de Canciones'].fillna(0).astype(int).apply(lambda x: 1 if pd.notna(x) and x != '' and x != 0 else x)
     for person in people[1:]:
-        # Añadir columnas vacías person_2 y person_3
+        # Añadir columnas vacías para el resto de personas (si las hay) 
         merged_df[person] = 0
         merged_df[person].astype(int)
 
@@ -35,8 +43,8 @@ def data_merge(schedule_df : pd.DataFrame, songs_df: pd.DataFrame, direction : s
     if drop:
         merged_df = merged_df[merged_df['Día'].isna()]
     else:
-        merged_df =reorganize(merged_df)
-
+        merged_df = reorganize(merged_df)
+        
     # Guardar el DataFrame resultante en un nuevo archivo CSV
     merged_df.to_csv(complete_rute, index=False)
 
